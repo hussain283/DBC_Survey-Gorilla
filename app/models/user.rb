1b95f7878b3ac5f6477b5cf76fd7694a1fceea2f
Apprentice <apprentice@devbootcamp.com>
2013-06-30 16:34:08 -0700

    Sinatra Authentication

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+  #TODO : Use bcrypt to store hashed passwords and authenticate users
+
+  validates :name, :email, :password, :presence => true
+  validates :email, :format => { :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/, :message => "Invalid Email" }
+  validates :email, uniqueness: { message: "Email already taken!"}
+  validates :password, :format => {:with => /^(?=.*\d)(?=.*[a-zA-Z]).{4,8}$/, :message => "Password must be 4-8 chars long and must contain one letter and one number"}, :on => :create
+  before_create :encrypt_password
+
+  def encrypt_password
+    self.password = BCrypt::Password.create(self.password).to_s
+  end
+
+  def self.authenticate(email, password)
+    User.all.each do |user| 
+      if (BCrypt::Password.new(user.password) == password) && (user.email == email)
+        p user
+        p BCrypt::Password.new(user.password)
+        return user
+      end
+    end
+    nil
+  end
+end
